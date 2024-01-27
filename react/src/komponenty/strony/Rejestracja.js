b57e235da00d78ef6a0ccb691a76197ca7d11241
Strzaa <strzaa@o2.pl>
2024-01-27 01:49:20 +0100

    test ci/cd

--- react/src/komponenty/strony/Rejestracja.js
+++ react/src/komponenty/strony/Rejestracja.js
@@ -2,5 +2,5 @@ import React from 'react'
 import '../../App.css'
 
 export default function Rejestracja(){
-    return <h1 className='Rejestracja'>REJESTRACJA</h1>
+    return <h1 className='Rejestracja'>REJESTRACJA TEST</h1>
 }
