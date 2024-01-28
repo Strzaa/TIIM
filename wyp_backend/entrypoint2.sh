9a2649bb9f23f77f0843a0e0a61f8c89c8e5a901
Strzaa <strzaa@o2.pl>
2024-01-28 15:04:25 +0100

    fixing django

--- wyp_backend/entrypoint2.sh
+++ wyp_backend/entrypoint2.sh
@@ -5,3 +5,4 @@ echo "Odpalam entrypoint"
 python manage.py makemigrations zarzadzanie_pojazdami
 python manage.py migrate
 python manage.py loaddata zarzadzanie_pojazdami/sample_data/pojazd.json
+python manage.py runserver
