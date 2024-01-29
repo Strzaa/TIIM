097fd6f7b4cfdc2bfb73cbe56a7207f4bf6f905f
Strzaa <strzaa@o2.pl>
2024-01-29 08:20:13 +0100

    entrypoint2

--- wyp_backend/entrypoint2.sh
+++ wyp_backend/entrypoint2.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-##
 
 sleep 5s
 echo "Odpalam entrypoint"
 python manage.py makemigrations zarzadzanie_pojazdami
 python manage.py migrate
+
 python manage.py loaddata zarzadzanie_pojazdami/sample_data/pojazd.json
+python manage.py loaddata zarzadzanie_pojazdami/sample_data/klient.json
+python manage.py loaddata zarzadzanie_pojazdami/sample_data/wypozyczenie.json
+
 python manage.py runserver 0.0.0.0:8000
